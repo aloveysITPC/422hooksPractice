cfe76d2580de18f4d3dfb3090c1f9e0560ff8c24
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-06-27 07:36:29 -0400

    useStateTime

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  // let time = new Date().toLocaleTimeString();
+
+  const [time, getTime] = useState(new Date().toLocaleTimeString());
+  console.log(time);
+
+  function sayHi() {
+    console.log("Hey");
+  }
+  setInterval(sayHi, 1000);
+
+  function currentTime() {
+    const timeNow = new Date().toLocaleTimeString();
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
+      <h1>{time}</h1>
       <button>Get Time</button>
     </div>
   );
